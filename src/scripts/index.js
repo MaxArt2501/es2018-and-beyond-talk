60518d54ad0305277196a739dce00fecf8fce218
Massimo Artizzu <massimo.artizzu@antreem.it>
2018-05-10 00:41:59 +0200

    Removed Reveal code 😑; added hljs init

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,24 +10,6 @@
       animationDelay: delay + 's'
     });
   });
+
+  hljs.initHighlightingOnLoad();
 })();
-
-// Full list of configuration options available at:
-// https://github.com/hakimel/reveal.js#configuration
-Reveal.initialize({
-  controls: true,
-  progress: true,
-  center: true,
-  history: true,
-
-  transition: 'slide', // none/fade/slide/convex/concave/zoom
-
-  // Optional reveal.js plugins
-  dependencies: [
-    { src: 'js/reveal/marked.js', condition: () => !!document.querySelector( '[data-markdown]' ) },
-    { src: 'js/reveal/markdown.js', condition: () => !!document.querySelector( '[data-markdown]' ) },
-    { src: 'js/reveal/highlight.js', async: true, callback() { hljs.initHighlightingOnLoad(); } },
-    { src: 'js/reveal/zoom.js', async: true },
-    { src: 'js/reveal/notes.js', async: true }
-  ]
-});
