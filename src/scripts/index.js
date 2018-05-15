ed89f92c4eff1b48f31e3f75ca773abea1ec394f
Massimo Artizzu <maxart.x@gmail.com>
2018-05-16 00:01:26 +0200

    Added a little console timer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,4 +12,39 @@
   });
 
   hljs.initHighlightingOnLoad();
+
+  const TIME_LIMIT = 420000;
+  let countDownTimer;
+  let countDownStart;
+  document.addEventListener('keydown', ({ code }) => {
+    if (code === 'KeyB') {
+      if (countDownStart) {
+        console.log('%c Countdown stopped at ' + getMinSec(), 'font-size: 2em');
+        clearInterval(countDownTimer);
+        countDownStart = null;
+      } else {
+        countDownTimer = setInterval(() => {
+          console.log('%c ' + getMinSec(), 'font-size: 2em; color: ' + getTimerColor());
+        }, 60000);
+        countDownStart = Date.now();
+        console.log('%c Countdown started', 'font-size: 2em');
+      }
+    }
+  });
+
+  function getMinSec() {
+    if (!countDownStart) {
+      return 'no time';
+    }
+    const diff = Math.round((Date.now() - countDownStart) / 1e3);
+    return Math.floor(diff / 60) + ':' + ('0' + (diff % 60)).slice(-2);
+  }
+  function getTimerColor() {
+    if (!countDownStart) {
+      return 'inherit';
+    }
+    const diff = Date.now() - countDownStart;
+    const hue = Math.max(0, TIME_LIMIT - diff) * 120 / TIME_LIMIT;
+    return `hsl(${hue}, 100%, 50%)`;
+  }
 })();
